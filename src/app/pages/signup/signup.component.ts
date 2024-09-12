f8bc2a8cd6ab150e2273186f529a236f1914338e
Camilla Avila <avilacamilla@outlook.com>
2024-09-12 16:37:13 -0300

    style: Add animation to the transition between login and signup pages

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormComponent, FormField } from '../../components/form/form.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],  // Importe o FormComponent reutilizável
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  // Defina os campos do formulário de cadastro
+  signupFields: FormField[] = [
+    {
+      label: 'Nome',
+      type: 'text',
+      name: 'name',
+      placeholder: 'Digite seu nome'
+    },
+    {
+      label: 'E-mail',
+      type: 'email',
+      name: 'email',
+      placeholder: 'Digite seu e-mail'
+    },
+    {
+      label: 'Senha',
+      type: 'password',
+      name: 'password',
+      placeholder: 'Crie uma senha'
+    }
+  ];
 
+  // Função para capturar o envio do formulário de cadastro
+  onSignup(formValues: any) {
+    console.log('Signup Form Values:', formValues);
+    // Aqui vai a lógica de cadastro
+  }
 }
