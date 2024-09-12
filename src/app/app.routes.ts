7c467b47369cc3eff8bae012f879cabc2b978258
Camilla Avila <avilacamilla@outlook.com>
2024-09-12 11:12:21 -0300

    feature: Config routes for login, signup, feed and new post pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { NewPostComponent } from './pages/new-post/new-post.component';
 
 
 export const routes: Routes = [
@@ -8,8 +11,24 @@ export const routes: Routes = [
     component: LoginComponent
   },
   {
+    path: 'signup',  // Rota para a página de signup
+    component: SignupComponent
+  },
+  {
+    path: 'feed',  // Rota para a página de feed
+    component: FeedComponent
+  },
+  {
+    path: 'new-post',  // Rota para a página de new-post
+    component: NewPostComponent
+  },
+  {
     path: '',
     redirectTo: '/login',
     pathMatch: 'full' // Redireciona para a página de login ao acessar a raiz
+  },
+  {
+    path: '**',
+    redirectTo: '/login' // Redireciona para a página de login caso a rota não exista
   }
 ];
