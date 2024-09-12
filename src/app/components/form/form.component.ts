1250db68f6e0d747d4455f68cd3932fd4fe98ecd
Camilla Avila <avilacamilla@outlook.com>
2024-09-12 14:46:35 -0300

    feature: Create and style reusable form component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
+
+export interface FormField {
+  label: string;
+  type: string;
+  name: string;
+  placeholder: string;
+  value?: string;
+}
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+  @Input() fields: FormField[] = [];  // Campos do formulário recebidos via Input
+  @Input() buttonLabel: string = '';  // Texto do botão
+  @Input() footerLink: string = '';   // Link do rodapé
+  @Input() footerLinkText: string = ''; // Texto do link de rodapé
+  @Input() title: string = ''; // Adicione um título opcional para o formulário
 
+  @Output() submitForm = new EventEmitter<any>();
+
+  onSubmit(formValues: any) {
+    this.submitForm.emit(formValues);
+  }
 }
