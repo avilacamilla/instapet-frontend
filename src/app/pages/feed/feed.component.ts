9617f6ab3a7529d18f3cc3410ff699e10691980a
Camilla Avila <avilacamilla@outlook.com>
2024-09-14 18:28:17 -0300

    feat: add upload modal logic to FeedComponent
    - Imported `UploadModalComponent` into `FeedComponent`.
    - Implemented methods to control the modal's visibility (`openModal` and `closeModal`).
    - Added `isModalOpen` property to manage the state of the modal.

--- src/app/pages/feed/feed.component.ts
+++ src/app/pages/feed/feed.component.ts
@@ -4,15 +4,30 @@ import { CardComponent } from '../../components/card/card.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ButtonAddComponent } from "./button-add/button-add.component";  
+import { UploadModalComponent } from './upload-modal/upload-modal.component';
+
 
 @Component({
   selector: 'app-feed',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, CardComponent, RouterModule, ButtonAddComponent], // Adicione RouterModule aqui
+  imports: [CommonModule, HeaderComponent, CardComponent, RouterModule, ButtonAddComponent, UploadModalComponent], // Importa o UploadModalComponent
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent {
+  // Controla a exibição do modal
+  isModalOpen: boolean = false;
+
+  // Método para abrir o modal
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  // Método para fechar o modal
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
   // Simulação de dados dos cards
   cards = [
     {
