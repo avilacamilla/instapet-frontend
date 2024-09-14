7fc9c02c75867cd2a67ed2438709612403379325
Camilla Avila <avilacamilla@outlook.com>
2024-09-14 19:39:42 -0300

    feat(feed): add logout method with slideRight animation
    - Implemented a logout method that navigates to the '/login' route with a slideRight animation effect.
    - Injected Angular Router to handle route navigation.

--- src/app/pages/feed/feed.component.ts
+++ src/app/pages/feed/feed.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Importa o Router
 import { HeaderComponent } from '../../components/header/header.component';
 import { CardComponent } from '../../components/card/card.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ButtonAddComponent } from "./button-add/button-add.component";  
 import { UploadModalComponent } from './upload-modal/upload-modal.component';
-
 
 @Component({
   selector: 'app-feed',
@@ -18,6 +18,8 @@ export class FeedComponent {
   // Controla a exibição do modal
   isModalOpen: boolean = false;
 
+  constructor(private router: Router) {} // Injeta o Router
+
   // Método para abrir o modal
   openModal() {
     this.isModalOpen = true;
@@ -26,6 +28,11 @@ export class FeedComponent {
   // Método para fechar o modal
   closeModal() {
     this.isModalOpen = false;
+  }
+
+  // Método para logout com animação slideRight
+  logout() {
+    this.router.navigate(['/login'], { state: { animate: 'slideRight' } });
   }
 
   // Simulação de dados dos cards
