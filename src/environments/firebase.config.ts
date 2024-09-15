61a240ac3e514e75f1d26cb0421b00230532f0a8
Camilla Avila <avilacamilla@outlook.com>
2024-09-15 08:41:24 -0300

    chore: successful deployment to Firebase Hosting

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBdFlTrjJcUgfWN-rZR0X8YFi-tDV5d7Rc",
+  authDomain: "instapet-angularjs.firebaseapp.com",
+  projectId: "instapet-angularjs",
+  storageBucket: "instapet-angularjs.appspot.com",
+  messagingSenderId: "931289655091",
+  appId: "1:931289655091:web:c4a6e361375d37e9648046"
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
+// main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { firebaseConfig } from './environments/firebase.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideStorage(() => getStorage()),
+    ...appConfig.providers
+  ]
+}).catch((err) => console.error(err));
